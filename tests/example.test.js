25dd3a1fd292dd467fa50986ee33765727309a12
Tyo Sulistya <tyo.sulistya@gada.io>
2023-12-16 17:32:34 +0800

    Interacting with Buttons & Checkbox

--- tests/example.test.js
+++ tests/example.test.js
@@ -9,9 +9,9 @@ describe('My First Puppeteer Test', () => {
         })
         const page = await browser.newPage()
         await page.goto('https://devexpress.github.io/testcafe/example/')
-        await page.type('#developer-name', 'Mike', {delay: 0 })
-        await page.waitFor(5000)
-        await browser.close()
+        await page.type('#developer-name', 'Mike', { delay: 0 })
+        await page.click('#tried-test-cafe', { clickCount: 1 })
+         await browser.close()
     }).timeout(10000);
 })
 
