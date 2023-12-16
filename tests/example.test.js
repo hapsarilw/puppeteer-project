a53cd8f7279f37516416efb44638711fe83a55ad
Tyo Sulistya <tyo.sulistya@gada.io>
2023-12-16 20:22:33 +0800

    Assertions

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
   
 describe('My First Puppeteer Test', () => {
     it('should launch the browser', async function(){
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: 'new',
             slowMo: 10,
             devtools: false,
         })
@@ -15,11 +16,14 @@ describe('My First Puppeteer Test', () => {
         const text = await page.$eval('h1', element => element.textContent)
         // $$ : multiple elemetnts
         const count = await page.$$eval('p', element => element.length)
-        console.log('Text in he H1: ' + text)
-        console.log('Number of P tags on the page:' + count)
+        
+        expect(title).to.be.a('string', 'Example Domain')
+        expect(url).to.include('example.com')
+        expect(text).to.be.a('string', 'Example Domain  ')
+        expect(count).to.equal(2)
         await browser.close()
     }).timeout(10000);
 })
-
+ 
 
       
