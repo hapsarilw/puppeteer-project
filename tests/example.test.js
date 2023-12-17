52b5fb77846be83ad2472054c8869b4d86c6dbc4
Tyo Sulistya <tyo.sulistya@gada.io>
2023-12-17 16:57:51 +0800

    Wait For XPath

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,31 +4,35 @@ const expect = require('chai').expect
 describe('My First Puppeteer Test', () => {
     it('should launch the browser', async function(){
         const browser = await puppeteer.launch({
-            headless: 'new',
+            headless: false,
             slowMo: 10,
             devtools: false,
         })
         const page = await browser.newPage()
-
-        // Default : 30s timeout
-        await page.setDefaultTimeout(10000)
+        await page.setDefaultTimeout(1000)
         await page.setDefaultNavigationTimeout(20000)
 
-        await page.goto('https://example.com/')
-        const title = await page.title('')
+        await page.goto('http://example.com/')
+        await page.waitForXPath('//h1')
+        const title = await page.title()
         const url = await page.url()
-        // $ : single elements
         const text = await page.$eval('h1', element => element.textContent)
-        // $$ : multiple elemetnts
-        const count = await page.$$eval('p', element => element.length)
-        
+        const count = await page.$eval('p', element => element.length)
+
         expect(title).to.be.a('string', 'Example Domain')
         expect(url).to.include('example.com')
-        expect(text).to.be.a('string', 'Example Domain  ')
-        expect(count).to.equal(2)
+        expect(text).to.be.a('string', 'Example Domain')
+        // expect(count).to.equal(2)
+
+        await page.goto('https://gh-users-search.netlify.app/')
+        await page.waitForSelector('[data-testid="search-bar"]')
+        await page.type('[data-testid="search-bar"]', 'hapsarilw');
+        await page.keyboard.press('Enter', {delay: 10})
+        //await page.waitFor(5000)
+
         await browser.close()
-    }).timeout(10000);
-})
+    }).timeout(5000)
+});
  
 
       
